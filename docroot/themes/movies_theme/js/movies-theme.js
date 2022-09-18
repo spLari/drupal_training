7f6450e191d71cfa51055353e925e3b2f7544a8f
Larisse Amorim <lpiresam@its.jnj.com>
2022-09-18 12:24:02 -0300

    Creating a custom theme

---
+++ docroot/themes/movies_theme/js/movies-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * movies_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.moviesTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
